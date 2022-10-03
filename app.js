5dfb3620e2bb936075cb62b130d665ad75eb9fdc
Renan Oliveira <65349948+renanbreno@users.noreply.github.com>
2022-10-03 10:21:34 -0300

    Melhorias de código

--- app.js
+++ app.js
@@ -21,29 +21,33 @@ const buildURL = cityName => {
     return `https://api.openweathermap.org/data/2.5/weather?${buildParams(params)}`;
 }
 
+const validateHTTPStatusCode = data => {
+    if (!data.ok) throw new Error(`HTTP error, status ${data.status}`);
+    return data.json();
+}
+
 const requestDataForecast = cityName => {
     fetch(buildURL(cityName))
-        .then(response => response.json())
-        .then(rec => showDataOnScreen(rec.main, rec.weather, rec.name))
+        .then(validateHTTPStatusCode)
+        .then(showDataOnScreen)
         .catch(error => console.log("Error", error))
 }
 
-const showDataOnScreen = (main, weather, cityName) => {
+const showDataOnScreen = ({ main, weather, name: cityName }) => {
     const contentWeather = document.querySelector(".content-weather");
-
-    console.log(main, weather)
+    const tempMinMax = `${main.temp_max.toFixed(0)}º / ${main.temp_min.toFixed(0)}`;
 
     if (weather[0].main === "Clouds") {
-        contentWeather.children[0].src = "img/cloudy.svg";
+        contentWeather.children[0].setAttribute('src', "img/cloudy.svg");
         contentWeather.children[1].innerText = "Hoje • Nublado";
-        contentWeather.children[2].innerText = `${main.temp_max.toFixed(0)}º / ${main.temp_min.toFixed(0)}`
+        contentWeather.children[2].innerText = tempMinMax;
     } else {
-        contentWeather.children[0].src = "img/icon _cloud rain_.svg";
+        contentWeather.children[0].setAttribute('src', "img/icon _cloud rain_.svg");
         contentWeather.children[1].innerText = "Hoje • Chuva";
-        contentWeather.children[2].innerText = `${main.temp_max.toFixed(0)}º / ${main.temp_min.toFixed(0)}`
+        contentWeather.children[2].innerText = tempMinMax;
     }
 
     document.getElementById("temp-content").innerText = main.temp.toFixed(0);
     document.getElementById("info-clouds").innerText = weather[0].description;
-    document.getElementById("cityName").innerHTML = cityName;
+    document.getElementById("cityName").innerText = cityName;
 }
